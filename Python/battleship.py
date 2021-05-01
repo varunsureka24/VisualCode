017d3aa59fdf57784d081cc63353ecc64fe182d3
varun <varunsureka123@gmail.com>
2021-05-01 11:27:16 -0500

    com

--- Python/battleship.py
+++ Python/battleship.py
@@ -3,9 +3,9 @@ import math
 import time
 
 #These are the three arrays I use for the various boards
-EnemyBoard = [100] #This is necessary to store the enemy ships. I randomize them and then place them in here
-MyBoard = [100] #This is necessary for the user's ships. After the user picks a position and direction for the ships, they are stored here
-ShotBoard = [100] #this is the array that the user shoots on to. This allows me to keep the enemy board secret
+EnemyBoard = [100] #stores enemy ships
+MyBoard = [100] #user ships
+ShotBoard = [100] #user shoots here
 
 #These are global variables that I use for counting if a ship has been sunk. 
 #E stands for enemy and these variables are called in the UserInput() function
@@ -23,118 +23,106 @@ USubmarineSunk = 0
 UDestroyerSunk = 0
 
 #These are colors for when I print the board
-RESET = "\u001B[0m" #this resets the color back to white
-RED = "\u001B[31m" #this sets the color to red. Used when you or the AI hits a ship
-BLUE = "\u001B[34m" #this sets the color to blue. Used when you or the AI misses the ship
+RESET = "\u001B[0m" #resets to white
+RED = "\u001B[31m" #sets color to red
+BLUE = "\u001B[34m" #sets color to blue
 
-def clearField(): #this fills the array with blank characters. This is necessary because it allows me 
-                  #to print a space for that spot when I print the boards
-                  #additionally, filling the boards here allows me to keep the boards global so that i can use them in other functions
+def clearField(): #intializes the arrays
     for i in range(100):
         EnemyBoard.insert(i, ' ')
         MyBoard.insert(i, ' ')
         ShotBoard.insert(i, ' ')
 
 def drawUserField(): # this is the function I use to print the user field
-    print("  .a.b.c.d.e.f.g.h.i.j.") # the string at the top that tells you the x coordinates
-
-    for i in range(100): #this is for the values in the array. There are 100 array spots and each one corrisponds to an array index
-        if (i % 10 == 0):
-            if (i != 0):
-                print("")
-
-            for dash in range(23): #this prints the lines between the rows
-                print("-", end="")
-
-            print("")
-
-            if ((i//10)+1 == 10): #this for loop allows me to print the numbers that indicate the y coordinates
-                print((i//10)+1, "|", end="", sep="")
-            else:
-                print((i//10)+1, " |", end="", sep="")
-        
-        marker = MyBoard[i] #this takes the value at each index in the array and stores it as a character
-        if (marker == 'X'): #if it is a hit, then the marker is colored red
-            print(RED + marker, end="")
-        elif (marker == 'O'): #if it is a miss, the marker is colored blue
-            print(BLUE + marker, end="")
-        else: 
-            print(marker, end="")
-        print(RESET + "|", end="") #then it resets the color of the marker to white
-
-    print("")
-    for dash in range(23):
-        print("-", end="")
-    print("")
-
-
-def drawShotBoard(): #this is the board that the user shoots on. it is destinct from the enemy board because 
-                     #i store the values for the enemy ships on the enemy board and I cant have those print out.
-    print("  .a.b.c.d.e.f.g.h.i.j.") #this code is extremly similar to the the code above because the grid is designed to look the same.
-
+    print("  .a.b.c.d.e.f.g.h.i.j.")
     for i in range(100):
         if (i % 10 == 0):
             if (i != 0):
                 print("")
-
             for dash in range(23):
                 print("-", end="")
-
             print("")
-
             if ((i//10)+1 == 10):
                 print((i//10)+1, "|", end="", sep="")
             else:
                 print((i//10)+1, " |", end="", sep="")
-        
-        marker = ShotBoard[i] #the main difference is the fact that i use the shot board array here
-        if (marker == 'X'):  #if the user hits an enemy ship it will be printed in red
+        marker = MyBoard[i]
+        if (marker == 'X'): #hit = red
             print(RED + marker, end="")
-        elif (marker == 'O'): #if the user misses the enemy, it will be printed in blue
+        elif (marker == 'O'): #miss = blue
             print(BLUE + marker, end="")
-        else:
+        else: 
             print(marker, end="")
-        print(RESET + "|", end="") #the color resets here
-
+        print(RESET + "|", end="") #resets to white
     print("")
     for dash in range(23):
         print("-", end="")
     print("")
 
-def positionCalc(spot): #this function calculates the position that the user has chosen. 
-    x = ord(spot[0].lower()) - 97 #this uses the ord() function which returns the ascii value for the character that you give it. 
-                                  #Then i subtract 97 from it because i need 'a' to equal 0 so that a1 is the first index of the array
-    y = int(spot[1]) #this converts the second character in the string 'shot' into an integer. It gives me the y value of the spot that they have chosen
-    position = ((y-1) * 10) + (x) #this equation converts the x and y values of where the user shot into a number out of 100. That number corresponds to the position of the shot in the array.
-    return position #returns the value of the shot.
+
+def drawShotBoard(): #where the user shoots            
+    print("  .a.b.c.d.e.f.g.h.i.j.") 
+    for i in range(100):
+        if (i % 10 == 0):
+            if (i != 0):
+                print("")
+            for dash in range(23):
+                print("-", end="")
+            print("")
+            if ((i//10)+1 == 10):
+                print((i//10)+1, "|", end="", sep="")
+            else:
+                print((i//10)+1, " |", end="", sep="")
+        marker = ShotBoard[i] #uses ShotBoard[] instead
+        if (marker == 'X'):  #hit = red
+            print(RED + marker, end="")
+        elif (marker == 'O'): #miss = blue
+            print(BLUE + marker, end="")
+        else:
+            print(marker, end="")
+        print(RESET + "|", end="") #the color resets here
+    print("")
+    for dash in range(23):
+        print("-", end="")
+    print("")
+
+
+def positionCalc(spot): #translates coordinates into a position
+    x = ord(spot[0].lower()) - 97 #calculates x position
+    y = int(spot[1]) #calculates y position
+    position = ((y-1) * 10) + (x) #calculates position
+    return position 
+
+
+
+
 
 #-------------------------------------------------# Enemy Ship Functions
-
-def EpositionChecker(position, lengthOfShip, direction, shipCharacter): #this is the primary algorithem for the enemy ships. It checks to see if there is enough space for an enemy ship to be placed
-                                        #it takes 4 parameters:
-                                            #position is where in the array the ship wants to be placed. This is randomized in the randomizeShips() function bellow.
-                                            #lengthOfShip is a constant that is provided in the randomizeShips() function. i.e. the carrier has a lengthOfShip value of 5, the battleship has a value of 4, etc.
-                                            #direction is the direction that the ship will point. it is important because it tells the fuction if it should check to the right of the position, if direction = 1, or bellow, if direction = 2
-                                            #shipCharacter is the character that will be printed for the ship. It is important because it allows the user and the computer to differentiate which ship is whihc. 
-                                            
-    for x in range(lengthOfShip): #runs for the length of the ship. It checks only the spots that the ship would fit in
-        if (direction == 1): #if direction == 1, then it is printing horizontally
-            if ((position % 10) + lengthOfShip > 10): #this if condition checks to see if the ship prints off of the side of the grid. The ship can only print in one row, it cant spill over to the next row
+#checks to see if there is enough space for an enemy ship to be placed
+def EpositionChecker(position, lengthOfShip, direction, shipCharacter): 
+    for x in range(lengthOfShip): 
+        if (direction == 1): #horizontal
+            #prevents ship from printing off side
+            if ((position % 10) + lengthOfShip > 10): 
                 return False
-            if (EnemyBoard[position +  x] != ' '): #this checks to see if the spots next to the ship aren't blank, i.e. there is another ship there
+                #checks if spaces are clear
+            if (EnemyBoard[position +  x] != ' '): 
                 return False
-        else: #if the ship is printing vertically
-            if ((math.floor(position / 10)) + lengthOfShip > 10): #this checks to make sure the ship doesn't print out of the grid vertically. Also chekcs to make sure the ship doesn't try to print in indexs that the array doesn't have preventing segmentation faults.
+        else: #vertical
+            #doesn't print out of the grid vertically.
+            if ((math.floor(position / 10)) + lengthOfShip > 10): 
                 return False
-
-            if (EnemyBoard[position + (x*10)] != ' '): #this make sures the spots bellow the ship are empty.
+            #spaces bellow the ship are empty.
+            if (EnemyBoard[position + (x*10)] != ' '): 
                 return False
-    EshipPlacer(position, lengthOfShip, direction, shipCharacter) #this calls another function which then fills the array with the ship
+    #fills the array with the ship
+    EshipPlacer(position, lengthOfShip, direction, shipCharacter) 
     return True
 
-def EshipPlacer(position, lengthOfShip, direction, shipCharacter): #this function prints the ship and is called in the function above. 
+#this function prints the ship and is called in the function above. 
+def EshipPlacer(position, lengthOfShip, direction, shipCharacter):
     for x in range(lengthOfShip): #it runs for the length of the ship
-        EnemyBoard[position] = shipCharacter #it prints the character that is associated with each ship
+        EnemyBoard[position] = shipCharacter
         if (direction == 1): #horizontal
             position +=1 #prints to the right
         else:              #vertical
@@ -143,10 +131,12 @@ def EshipPlacer(position, lengthOfShip, direction, shipCharacter): #this functio
 
 def randomizeShips(): #this function randomizes the ships. 
     tempBool = False
-    while(not tempBool): #these while loops guarantee that the ship is placed correctly
+    #these while loops guarantee that the ship is placed correctly
+    while(not tempBool):
         ECarrierPos = random.randint(0,99)
         ECarrierDir = random.randint(1,2)
-        tempBool = EpositionChecker(ECarrierPos, 5, ECarrierDir, 'C') #this is where i check to see if the ship can be placed there
+        #this is where i check to see if the ship can be placed there
+        tempBool = EpositionChecker(ECarrierPos, 5, ECarrierDir, 'C') 
     
     tempBool = False
     while(not tempBool):
@@ -177,32 +167,44 @@ def randomizeShips(): #this function randomizes the ships.
 #-------------------------------------------------# User Ship Functions
 
 #main algorithem
-def UpositionChecker(position, lengthOfShip, direction, shipCharacter): #this function checks to see if the user can legally place their ships at the spot that they pick. 
+#this function checks to see if the user can place ships where they picked.
+def UpositionChecker(position, lengthOfShip, direction, shipCharacter):  
     if (direction == 1): #vertical
-        for x in range(lengthOfShip): #it checks the amount of spaces that the ship would take up
-            if (MyBoard[position + (x*10)] != ' '): #checks to see if the spots bellow are clear
+        #it checks the amount of spaces that the ship would take up
+        for x in range(lengthOfShip):
+            #checks to see if the spots bellow are clear
+            if (MyBoard[position + (x*10)] != ' '): 
                 return False
     else: #horizontal
-        for x in range(lengthOfShip): #it checks the amount of spaces that the ship would take up
-            if (MyBoard[position +  x] != ' '): #checks to see if the spots to the right of the ship are clear
+        #it checks the amount of spaces that the ship would take up
+        for x in range(lengthOfShip):
+            #checks to see if the spots to the right of the ship are clear
+            if (MyBoard[position +  x] != ' '):
                 return False
-    UshipPlacer(position, lengthOfShip, direction, shipCharacter) #calls another function to fill the array with the ship values
-    return True 
-
-def UshipPlacer(position, lengthOfShip, direction, shipCharacter): #this is the function that is called above
-    for x in range(lengthOfShip): #it runs for the length of the ship
+    #fills the array
+    UshipPlacer(position, lengthOfShip, direction, shipCharacter) 
+    return True
+    
+     
+#fills array
+def UshipPlacer(position, lengthOfShip, direction, shipCharacter):
+    #it runs for the length of the ship
+    for x in range(lengthOfShip):
         if (direction == 1): #vertical
-            MyBoard[position + (x*10)] = shipCharacter #fills the array with the ship values vertically
+            #fills the array with the ship values vertically
+            MyBoard[position + (x*10)] = shipCharacter 
         else: #horizontal
-            MyBoard[position + x] = shipCharacter #fills the array with the ship values horizontally
+            #fills the array with the ship values horizontally
+            MyBoard[position + x] = shipCharacter 
 
 
 #-------------------------------------------------# Shooting Functions
+#this function uses where the user shoots to determine if they can shoot there
+#also determines if you sink any enemy ships
+def UserInput(shot): 
+    position = positionCalc(shot) 
 
-def UserInput(shot): #this function uses where the user shoots to determine if they can shoot there, and then it also checks to see if you have sunk any of their ships
-    position = positionCalc(shot) #calculates the position for where the user shot.
-
-    #these variables have to be declared as global so that I can modify them here.
+    #global so that I can modify them here.
     #they determine if any of the ships have been sunk
     global ECarrierSunk
     global EBattleshipSunk
@@ -210,7 +212,9 @@ def UserInput(shot): #this function uses where the user shoots to determine if t
     global ESubmarineSunk
     global EDestroyerSunk
 
-    if (ShotBoard[position] == 'X' or ShotBoard[position] == 'O'): #if the user has already shot at they position that they picked
+
+    #if the user has already shot at they position that they picked
+    if (ShotBoard[position] == 'X' or ShotBoard[position] == 'O'): 
         print("you already shot there!")
         time.sleep(1)
         return False
@@ -253,10 +257,12 @@ def UserInput(shot): #this function uses where the user shoots to determine if t
             ShotBoard[position] = 'O'
         return True
 
-def AIshoots(): #this function determines where the AI shoots
-    position = random.randint(0,99) #picks a random spot between 1 and 99
+#this function determines where the AI shoots
+def AIshoots():
+    #picks a random spot between 1 and 99
+    position = random.randint(0,99)
 
-    #these variables have to be declared as global so that I can modify them here.
+    #global so that I can modify them here.
     #they determine if any of the ships have been sunk
     global UCarrierSunk 
     global UBattleshipSunk
@@ -264,7 +270,8 @@ def AIshoots(): #this function determines where the AI shoots
     global USubmarineSunk
     global UDestroyerSunk
 
-    while (MyBoard[position] == 'X' or MyBoard[position] == 'O'): #if the AI shoots in a spot that it has already shot, it will move over to the right and test again
+    #if the AI shoots in a spot that it has already shot, it will move over to the right and test again
+    while (MyBoard[position] == 'X' or MyBoard[position] == 'O'):
         if (position > 99):
             position = 0
         else:
--- Python/battleshipClient.py
+++ Python/battleshipClient.py
@@ -7,35 +7,36 @@ battleship.drawUserField() #prints the user's board
 
 #these while loops ask the user for a position to place their ship, and then a direction
 tempBool = False
-while(not tempBool):
+while(not tempBool): #carrier
     ShipPos = input("Where do you want to place your carrier (5 long)? Enter a start point (enter the column then the row. Example: a1 or f8)")
     ShipDir = input("Enter a direction (\u001B[31m1\u001B[0m is vertical and goes down. \u001B[31m2\u001B[0m is horizontal and goes to the right)")
-    tempBool = battleship.UpositionChecker(battleship.positionCalc(ShipPos), 5, int(ShipDir), 'C') #this statement checks to see if the ship can be printed there
+    #this statement checks to see if the ship can be printed there
+    tempBool = battleship.UpositionChecker(battleship.positionCalc(ShipPos), 5, int(ShipDir), 'C')
 battleship.drawUserField()
 
 tempBool = False
-while(not tempBool):
+while(not tempBool): #battleship
     ShipPos = input("Where do you want to place your battleship (4 long)? Enter a start point (enter the column then the row. Example: a1 or f8)")
     ShipDir = input("Enter a direction (\u001B[31m1\u001B[0m is vertical and goes down. \u001B[31m2\u001B[0m is horizontal and goes to the right)")
     tempBool = battleship.UpositionChecker(battleship.positionCalc(ShipPos), 4, int(ShipDir), 'B')
 battleship.drawUserField()
 
 tempBool = False
-while(not tempBool):
+while(not tempBool): #cruiser
     ShipPos = input("Where do you want to place your carrier (3 long)? Enter a start point (enter the column then the row. Example: a1 or f8)")
     ShipDir = input("Enter a direction (\u001B[31m1\u001B[0m is vertical and goes down. \u001B[31m2\u001B[0m is horizontal and goes to the right)")
     tempBool = battleship.UpositionChecker(battleship.positionCalc(ShipPos), 3, int(ShipDir), 'c')
 battleship.drawUserField()
 
 tempBool = False
-while(not tempBool):
+while(not tempBool): #submarine
     ShipPos = input("Where do you want to place your submarine (3 long)? Enter a start point (enter the column then the row. Example: a1 or f8)")
     ShipDir = input("Enter a direction (\u001B[31m1\u001B[0m is vertical and goes down. \u001B[31m2\u001B[0m is horizontal and goes to the right)")
     tempBool = battleship.UpositionChecker(battleship.positionCalc(ShipPos), 3, int(ShipDir), 'S')
 battleship.drawUserField()
 
 tempBool = False
-while(not tempBool):
+while(not tempBool): #destroyer
     ShipPos = input("Where do you want to place your destroyer (4 long)? Enter a start point (enter the column then the row. Example: a1 or f8)")
     ShipDir = input("Enter a direction (\u001B[31m1\u001B[0m is vertical and goes down. \u001B[31m2\u001B[0m is horizontal and goes to the right)")
     tempBool = battleship.UpositionChecker(battleship.positionCalc(ShipPos), 2, int(ShipDir), 'D')
@@ -48,11 +49,13 @@ battleship.drawShotBoard() #this draws the board that the user will shoot on.
 enemyShipsRemaining = True
 myShipsRemaining = True
 
-while (enemyShipsRemaining and myShipsRemaining): #while there are still ships remaining
+#while there are still ships remaining
+while (enemyShipsRemaining and myShipsRemaining):
     EnemyShipCounter = 0
     UserShipCounter = 0
 
-    while(True): #asks the user where they want to shoot and tests to see if they can shoot there
+    #asks user where they want to shoot and tests to see if they can shoot there
+    while(True):
         shot = input("Where do you want to shoot?")
         if (battleship.UserInput(shot)):
             break
@@ -73,12 +76,14 @@ while (enemyShipsRemaining and myShipsRemaining): #while there are still ships r
     for x in range(100):
         if (battleship.ShotBoard[x] == 'X'):
             EnemyShipCounter +=1
-            if (EnemyShipCounter == 17): #if EnemyShipCounter == 17, then you have sunk all of the enemy ships and the game is over
+            #tests if you have sunk all of the enemy
+            if (EnemyShipCounter == 17):
                 enemyShipsRemaining = False
                 print("you have won!")
 
         elif(battleship.MyBoard[x] == 'x'):
             UserShipCounter += 1
-            if(UserShipCounter == 17): #if UserShipCounter == 17, then the AI has sunk all of your ships and the game is over
+            #tests if AI has sunk all of your ships
+            if(UserShipCounter == 17): 
                 myShipsRemaining = False
                 print("the AI has won!")
